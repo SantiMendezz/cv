32c30b902abbb518071466409d5a2327aa5b7125
SantiMendezz <santimendezsam@gmail.com>
2025-05-02 17:18:30 -0300

    Cambio en el base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // Cambia esto si se despliega en subdirectorio
   plugins: [react()],
+  base: 'https://santimendezz.github.io/cv',  // Cambia esto si se despliega en subdirectorio
 })
