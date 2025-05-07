69db8bd1f2a3acc8757e131e3924aaa103400f21
SantiMendezz <santimendezsam@gmail.com>
2025-05-07 19:18:22 -0300

    Agregado titulo y svg al index.html para - v1.0.0

--- src/components/sections/Educacion.jsx
+++ src/components/sections/Educacion.jsx
@@ -4,11 +4,20 @@ import styles from '../../css/educacion.module.css'
 import logoIEN from '/img/ien-logo.jpg'
 import logoCodo from '/img/codoAcodo-logo.png'
 import logoArgProg from '/img/argPrograma-logo.png'
+import UNNE from '/img/LogoUnne_solo.png'
 
 export const Educacion = () => {
   return (
     <div className={styles['educacion-container']} id='educacion'>
         <h2>Educación</h2>
+        <div className={styles['educacion-item']}>
+            <div className={styles['cursos']}>
+                <h3>Universidad Nacional del Nordeste (UNNE)</h3>
+                <p><span>Analista Programador Universitario</span> - (94.44 % de avance)</p>
+                <p><span>Licenciatura en Sistemas de Información</span>- (68.97 % de avance)</p>
+            </div>
+            <img src={UNNE} className={styles['logo-institucion']} alt='Codo a Codo 4.0'/>
+        </div>
         <div className={styles['educacion-item']}>
             <div className={styles['cursos']}>
                 <h3>Codo a Codo 4.0</h3>
